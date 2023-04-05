6faa9bdfeb71ce9d7cfd74711d642b99e4883ed9
ay-eonii <naayen@naver.com>
2023-04-05 15:42:54 +0900

    refactor: 세션로그인_어노테이션 기반으로 개선

---
+++ src/main/java/com/jojoldu/book/springboot/config/auth/LoginUser.java
@@ -0,0 +1,11 @@
+package com.jojoldu.book.springboot.config.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER) //어노테이션 위치
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginUser {
+}
